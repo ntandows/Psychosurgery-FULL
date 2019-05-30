f795af3987fdb86554e89727e565f38e9f0eae17
Matt Briones <mbriones@uoregon.edu>
2019-05-30 14:44:19 -0700

    Almost done with first level

---
+++ Assets/Code/OurScripts/InteractOnButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractOnButton : MonoBehaviour
+{
+    private GameController control;
+    private bool isHit;
+
+    private void Start()
+    {
+        isHit = false;
+        control = gameObject.GetComponent(typeof(GameController)) as GameController;
+        control.PrintDiagnostics();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.E) && !isHit)
+        {
+            control.LessCheckpoint();
+            isHit = true;
+            print("Checkpoint hit");
+        }
+    }
+
+
+}
